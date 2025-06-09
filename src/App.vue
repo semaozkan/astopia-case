4f513738ad95a829f20833ee1fb6e016efb04cb9
Semanur Özkan <semanurozkan4444@gmail.com>
2025-06-09 12:35:47 +0300

    feat(app): import components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <script setup>
+import TopBanner from "./components/TopBanner.vue";
+import Navbar from "./components/Navbar.vue";
 
 </script>
 
 <template>
+  <div class="container">
+    <TopBanner />
+    <Navbar />
+    <router-view />
+  </div>
 
 </template>
 
+<style scoped>
+.container {
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>

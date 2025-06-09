8af18d630bf0d4de4374b54ad70f737a2f002037
Semanur Özkan <semanurozkan4444@gmail.com>
2025-06-09 20:46:29 +0300

    feat(formInfo): create form-info component

---
+++ src/components/FormInfo.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <img src="../assets/form_info.png" alt="">
+        <div class="info">
+            <p>
+                Your Rising Sign reveals how others see you, how you respond instinctively, and how you move through the
+                world. It's the cover of your cosmic story, setting the tone before a single word of your soul is
+                read.
+            </p>
+            <br>
+
+            <p>
+                Find out your Ascendant now with Astopia and take the first step toward understanding this
+                essential part of your astrological identity.
+            </p>
+            <br>
+            <p>
+                <span class="highlight">If you have an account, you should log in here.</span>If you don't have an
+                account, you should fill in the
+                following information to learn your rising sign.
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+    padding: 11.5px 0;
+}
+
+.container img {
+    max-width: 334px;
+}
+
+.info {
+    font-family: "Poppins", sans-serif;
+    max-width: 368px;
+    color: white;
+    font-size: 15px;
+    font-weight: 400;
+}
+
+
+.highlight {
+    color: #FCDAA2;
+}
+</style>

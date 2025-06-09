d23bb98fba9c6af7fa68584d3ceb236e4418752c
Semanur Özkan <semanurozkan4444@gmail.com>
2025-06-09 12:32:05 +0300

    feat: create top-banner component

---
+++ src/components/TopBanner.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="top-banner">
+    Your 2025 Annual Forecast Await You – Exclusively on Astopia!
+  </div>
+</template>
+
+
+<style scoped>
+.top-banner {
+  width: 100%;
+  height: 51px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 15px 10px;
+  font-family: "Playfair Display", serif;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 100%;
+  letter-spacing: 0%;
+  color: #F9FBFC;
+  /* background: linear-gradient(135deg, rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.2)); */
+
+  background: linear-gradient(135deg,
+      rgba(255, 255, 255, 0.15) 0%,
+      rgba(255, 255, 255, 0.2) 40%,
+      rgba(255, 255, 255, 0.1) 100%);
+
+  backdrop-filter: blur(2px);
+  -webkit-backdrop-filter: blur(2px);
+
+  margin-bottom: 50px;
+}
+
+@media (max-width: 768px) {
+  .top-banner {
+    width: 100vw;
+    max-width: 100%;
+    margin-bottom: 20px;
+    font-size: 14px;
+    padding: 12px 8px;
+  }
+}
+</style>
